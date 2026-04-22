2617e0e748ef99a551ef450d2a840ba2c6ca8a9e
Gustavo Zocharato Ferreira <gu.zocharato@gmail.com>
2026-04-22 16:59:09 -0300

    remove basePath for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,10 @@
 import type { NextConfig } from "next";
 
-const repo = "/meu-portfolio";
-
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: repo,
-  assetPrefix: `${repo}/`,
-  trailingSlash: true,
 };
 
 export default nextConfig;
