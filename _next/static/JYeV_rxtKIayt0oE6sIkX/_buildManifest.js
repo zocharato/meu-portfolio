7883c19bf4466b761faf273c8af5cae03412290d
Gustavo Zocharato Ferreira <gu.zocharato@gmail.com>
2026-04-22 16:33:22 -0300

    Updates

--- _next/static/JYeV_rxtKIayt0oE6sIkX/_buildManifest.js
+++ _next/static/JYeV_rxtKIayt0oE6sIkX/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/meu-portfolio//_next/:path+",
+        "destination": "/meu-portfolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
